89944c6c2afe4d54a559bbbdf07ae673a3553d55
Jayasekara Kapukotuwa <ncbdrck@gmail.com>
2023-11-06 21:33:35 +0000

    added ros_kinematics

--- src/ros_rl/__init__.py
+++ src/ros_rl/__init__.py
@@ -1,4 +1,4 @@
 from ros_rl.envs import RealBaseEnv, RealGoalEnv
-from ros_rl.utils import moveit_ros_rl, ros_common, ros_controllers, ros_markers
+from ros_rl.utils import moveit_ros_rl, ros_common, ros_controllers, ros_markers, ros_kinematics
 from ros_rl.wrappers import normalize_action_wrapper, normalize_obs_wrapper, time_limit_wrapper
 from ros_rl import core
