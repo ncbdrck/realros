0728e4176fabc49f6d93e1eb0a9cb5af8a0a2190
Jayasekara Kapukotuwa <ncbdrck@gmail.com>
2023-12-13 08:55:57 +0000

    added make

--- src/realros/__init__.py
+++ src/realros/__init__.py
@@ -2,3 +2,7 @@ from realros.envs import RealBaseEnv, RealGoalEnv
 from realros.utils import moveit_realros, ros_common, ros_controllers, ros_markers, ros_kinematics
 from realros.wrappers import normalize_action_wrapper, normalize_obs_wrapper, time_limit_wrapper
 from realros import core
+
+# To use this package similar to gym.make
+from realros.core import realros_gym
+make = realros_gym.make
